defaaeddc5d726c13e34d3e6acb9e3cac1d779af
tenworx <tenworx.development@gmailcom>
2022-10-06 05:14:52 -0700

    welcome and splash screen

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -31,7 +31,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     super.initState();
     setPrefrences().then((_) {
       setState(() {});
-      prefs.setString("welcome", "done");
+      // prefs.setString("welcome", "done");
     });
   }
 
@@ -40,80 +40,92 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
       body: Container(
         color: kPrimaryColor,
-        child: Column(
+        child: Stack(
           children: [
-            const Spacer(
-              flex: 2,
-            ),
             Container(
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white, width: 3),
-                  borderRadius: BorderRadius.circular(5)),
-              child: const Text(
-                'ChatInUni',
-                style: TextStyle(fontSize: 30, color: Colors.white),
-              ),
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-            Image.asset(
-              "assets/images/welcome.png",
+              height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
-              // fit: BoxFit.fill,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      opacity: 0.04,
+                      image: AssetImage('assets/images/ko-01.png'),
+                      fit: BoxFit.cover)),
             ),
-            const Spacer(
-              flex: 1,
-            ),
-            Text(
-              "skip_screen_exp".tr,
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5
-                  ?.copyWith(fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-            const Spacer(),
-            Text(
-              "skip_screen_sub_exp".tr,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
-            ),
-            const Spacer(
-              flex: 3,
-            ),
-            FittedBox(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChatByStatus(
-                        flag: false,
-                      ),
-                    ),
-                  );
-                },
-                child: Row(
-                  children: [
-                    Text(
-                      "skip".tr,
-                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                            color: Colors.white,
-                          ),
-                    ),
-                    const SizedBox(
-                      width: kDefaultPadding / 4,
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      size: 16,
-                      color: Colors.white,
-                    ),
-                  ],
+            Column(
+              children: [
+                const Spacer(
+                  flex: 2,
                 ),
-              ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 3),
+                      borderRadius: BorderRadius.circular(5)),
+                  child: const Text(
+                    'ChatInUni',
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                Image.asset(
+                  "assets/images/welcome.png",
+                  width: MediaQuery.of(context).size.width,
+                  // fit: BoxFit.fill,
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+                Text(
+                  "skip_screen_exp".tr,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline5?.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                const Spacer(),
+                Text(
+                  "skip_screen_sub_exp".tr,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+                const Spacer(
+                  flex: 3,
+                ),
+                FittedBox(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatByStatus(
+                            flag: false,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          "skip".tr,
+                          style:
+                              Theme.of(context).textTheme.bodyText1?.copyWith(
+                                    color: Colors.white,
+                                  ),
+                        ),
+                        const SizedBox(
+                          width: kDefaultPadding / 4,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
